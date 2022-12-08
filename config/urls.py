55c2ca919985248c9ebb1a72f81b2ed5ef82b2e4
s1f10210223 <s1f102102234@iniad.org>
2022-12-08 17:21:22 +0900

    authtest

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include("chat.urls")),
+    path('app01/', include('authtest.urls')),
     path('admin/', admin.site.urls),
 ]
