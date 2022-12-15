02436d885e45fd27b34e3f5cb16492a16ecd0ac9
s1f102101154 <85399300+s1f102101154@users.noreply.github.com>
2022-12-15 17:20:11 +0900

    urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    #ヘッダー
     path("", views.index, name="index"),
+    path("weather", views.weather, name="weather"),
+    path("politics", views.politics, name="politics"),
+    path("sport", views.sport, name="sport"),
+    path("it", views.it, name="it"),
+    path("animal", views.animal, name="animal"),
+
+    #記事
+    path("news1", views.news1, name="news1"),
+    path("news2", views.news1, name="news2"),
+    path("news3", views.news1, name="news3"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -3,3 +3,27 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "chat/index.html")
+
+def weather(request):
+    return render(request, "chat/weather.html")
+
+def politics(request):
+    return render(request, "chat/politics.html")
+
+def sport(request):
+    return render(request, "chat/sport.html")
+
+def it(request):
+    return render(request, "chat/it.html")
+
+def animal(request):
+    return render(request, "chat/animal.html")
+
+def news1(request):
+    return render(request, "news/news1.html")
+
+def news2(request):
+    return render(request, "news/news2.html")
+
+def news3(request):
+    return render(request, "news/news3.html")
