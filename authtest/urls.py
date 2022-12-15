b87c4f0d09deb8e5da20d809dbe68775fcf0b74e
s1f10210223 <s1f102102234@iniad.org>
2022-12-15 17:11:41 +0900

    認証が必要なページと不要なページ(仮)

--- authtest/urls.py
+++ authtest/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('priv', views.private_page, name='priv'),
+    path('pub', views.public_page, name='pub'),
 ]
--- authtest/views.py
+++ authtest/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'authtest/home.html', {})
+
+@login_required
+def private_page(request):
+    return render(request, 'authtest/private.html', {})
+
+def public_page(request):
+    return render(request, 'authtest/public.html', {})
