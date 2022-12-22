e2afab596f19942ac493d47509f4f3c6bf369f02
s1f10210223 <s1f102102234@iniad.org>
2022-12-22 17:33:38 +0900

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -51,6 +51,6 @@ class IndexView2(generic.TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexView2, self).get_context_data(**kwargs)
         newsapi = NewsApiClient(api_key=settings.NEWSAPI)
-        context['top_headlines'] = newsapi.get_everything(categry='sports')
+        context['top_headlines'] = newsapi.get_everything(category='sports')
         print(context['top_headlines'])
         return context
