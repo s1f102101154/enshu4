7fa4cf279eac3fdf8febe5534d24f5db0fa77494
s1f10210223 <s1f102102234@iniad.org>
2022-12-22 17:26:59 +0900

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("weather", views.weather, name="weather"),
     path("politics", views.politics, name="politics"),
-    path("sport", views.sport, name="sport"),
+    path("sport", views.IndexView2.as_view(), name="sport"),
     path("it", views.it, name="it"),
     path("animal", views.animal, name="animal"),
 
