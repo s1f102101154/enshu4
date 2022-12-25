036418452d9fe3be3b8a6c4718ab6058ac0581e5
s1f102101154 <85399300+s1f102101154@users.noreply.github.com>
2022-12-25 16:53:30 +0900

    update

--- chat/views.py
+++ chat/views.py
@@ -40,7 +40,7 @@ class IndexView(generic.TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         newsapi = NewsApiClient(api_key=settings.NEWSAPI)
-        context['top_headlines'] = newsapi.get_everything(q='キンプリ')
+        context['top_headlines'] = newsapi.get_everything(domains = "yahoo.co.jp",page_size = 5)
         print(context['top_headlines'])
         return context
 #
