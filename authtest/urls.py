2adffe219f2218261dd4229dce64b1ec9fee6b32
s1f102101154 <85399300+s1f102101154@users.noreply.github.com>
2023-01-09 20:42:28 +0900

    ok

--- authtest/urls.py
+++ authtest/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path("priv", views.IndexView.as_view(), name="priv"),
-    path('pub', views.public_page, name='pub'),
+    path("priv", views.IndexView2.as_view(), name="priv"),
+    path('pub', views.IndexView.as_view(), name='pub'),
+    path("", views.IndexView.as_view(), name="index"),
 ]
--- authtest/views.py
+++ authtest/views.py
@@ -11,16 +11,24 @@ def home(request):
     return render(request, 'authtest/home.html', {})
 
 #@login_required
-class IndexView(LoginRequiredMixin,generic.TemplateView):
-    template_name = "authtest/private.html"
-    login_url = 'authtest/index.html'
+class IndexView(generic.TemplateView):
+    template_name = "authtest/public.html"
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
+        newsapi = NewsApiClient(api_key=settings.NEWSAPI)
+        context['top_headlines'] = newsapi.get_everything(domains = "yahoo.co.jp",page_size = 5)
+        print(context['top_headlines'])
+        return context
+
+class IndexView2(LoginRequiredMixin,generic.TemplateView):
+    template_name = "authtest/private.html"
+    #login_url = 'authtest/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView2, self).get_context_data(**kwargs)
         newsapi = NewsApiClient(api_key=settings.NEWSAPI)
         context['top_headlines'] = newsapi.get_everything(domains = "yahoo.co.jp")
         print(context['top_headlines'])
         return context
 
-def public_page(request):
-    return render(request, 'authtest/index.html', {})
