7e1dbb0c5554b6f938e86c96ff336498e017ca4d
s1f102101154 <85399300+s1f102101154@users.noreply.github.com>
2023-01-09 15:39:32 +0900

    ?

--- authtest/urls.py
+++ authtest/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('priv', views.private_page, name='priv'),
+    path('priv', views.IndexView.as_view(), name='priv'),
     path('pub', views.public_page, name='pub'),
 ]
--- authtest/views.py
+++ authtest/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from django.views import generic
+from django.conf import settings
+from newsapi import NewsApiClient
+
 # Create your views here.
 def home(request):
     return render(request, 'authtest/home.html', {})
 
 @login_required
-def private_page(request):
-    return render(request, 'chat/templates/news/news1.html', {})
+class IndexView(generic.TemplateView):
+    template_name = "authtest/private.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        newsapi = NewsApiClient(api_key=settings.NEWSAPI)
+        context['top_headlines'] = newsapi.get_everything(domains = "yahoo.co.jp")
+        print(context['top_headlines'])
+        return context
+"""def private_page(request):
+    return render(request, 'authtest/private.html', {})"""
 
 def public_page(request):
-    return render(request, 'chat/templates/news/news2.html', {})
+    return render(request, 'authtest/index.html', {})
